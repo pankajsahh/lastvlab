24ccf279aed8dfb00057833f859d8c62b643d37d
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2022-06-28 10:38:23 +0530

    Quiz completed.

--- insertion_sort/src/Components/PostTest.js
+++ insertion_sort/src/Components/PostTest.js
@@ -3,13 +3,33 @@ import './PostTest.css';
 import  crown from './crown.png'
 import quiz from './quiz.png'
 function PostTest(){
+  let ch=<>
+  <ul className="list-disc list-inside">
+    <li className="font-bold text-left text-lg">This quiz consists of three questions.</li>
+    <li className="font-bold text-left text-lg">Four options will be given to each question.</li>
+    <li className="font-bold text-left text-lg">You cannot change your answer once it is selected.</li>
+    <li className="font-bold text-left text-lg">You cannot move to next question before attempting the current question.</li>
+    <li className="font-bold text-left text-lg">At the end of the quiz, your score will be displayed.</li>
+    <li className="font-bold text-left text-lg">Move to next question by clicking "Next" button.</li>
+    <li className="font-bold text-left text-lg">Restart the quiz by clicking "Try Again" button.</li>
+    <li className="font-bold text-left text-lg">Start the quiz by clicking "Start Quiz" button.</li>
+
+    </ul></>
     const question=[
+      {
+        questionText:ch,
+        
+        answersOptions:[
+          {answerText:'Start Quiz',isCorrect:true}
+        ]
+      }
+      ,
         {
-          questionText: 'What is the running time of insertion sort if the input is reverse-sortoed?',
+          questionText: 'What is the running time of insertion sort if the input is reverse-sorted?',
           answersOptions:[
-            {answerText: 'O(N^2)',isCorrect: false},
+            {answerText: 'O(N^2)',isCorrect: true},
             {answerText: 'O(N log N)',isCorrect: false},
-            {answerText: 'O(N)',isCorrect: true},
+            {answerText: 'O(N)',isCorrect: false},
             {answerText: 'O(M log N)',isCorrect: false},
           ],
         },
@@ -36,48 +56,109 @@ function PostTest(){
       const [currentQuestion, setCurrentQuestion]=useState(0);
       const [showScore, setScore]=useState(false);
       const [score,setfinalScore]=useState(0);
-      const handleButtonClick = (isCorrect) =>{
+      const [visiblity,setVisiblity]=useState('invisible');
+
+      // handle selected answer 
+      const handleButtonClick = (isCorrect,ind) =>{
+        // console.log("id is ",ind.target.id)
+        // enable next button after answer selection 
+        document.getElementById('nxt').disabled=false;
+
+        // disable options once one is selected 
+        const btns= document.querySelectorAll('.btn');
+        btns.forEach(btn=>btn.disabled=true)
+
           if(isCorrect===true){
               setfinalScore(score+1);
-            
+              document.getElementById(ind.target.id).style.backgroundColor="Green";
+          }else document.getElementById(ind.target.id).style.backgroundColor="Red";
+
+          // mark the correct option on selection of wrong answer 
+          question[currentQuestion].answersOptions.filter((value,index)=>{
+            if(value.isCorrect){
+              document.getElementById(index).style.backgroundColor="Green"
+            }
+          })
+          
+          if(currentQuestion==0){
+            nextQue();
           }
-          const nextQuestion=currentQuestion+1;
-          if(nextQuestion<question.length){
-            setCurrentQuestion(nextQuestion);
-           
-          }else{
-              setScore(true); 
-          }
+          
+      }
+      // move to next question 
+      const nextQue=()=>{
+        // enable the option selection 
+        const btns= document.querySelectorAll('.btn');
+        btns.forEach(btn=>btn.disabled=false)
+
+        // set bg colour of all the options to the default 
+        if(currentQuestion!=0){
+        document.getElementById(0).style.backgroundColor="#252d4a";
+        document.getElementById(1).style.backgroundColor="#252d4a";
+        document.getElementById(2).style.backgroundColor="#252d4a";
+        document.getElementById(3).style.backgroundColor="#252d4a";
+        // document.querySelectorAll('.btn').disabled=false;
+
+        }else document.getElementById(0).style.backgroundColor="#252d4a";
+
+        if(currentQuestion==0){
+          setVisiblity('visible')
+        }
+
+        const nextQuestion=currentQuestion+1;
+        if(nextQuestion<question.length){
+          
+          setCurrentQuestion(nextQuestion); 
+          
+        }else{
+            setScore(true); 
+        }
+        // disable next button before answer selection 
+        document.getElementById('nxt').disabled=true;
+      }
+
+      // function to restart the quiz 
+      const restart =()=>{
+        setScore(false);
+        setCurrentQuestion(0);
+        setfinalScore(0);
+        setVisiblity('invisible');
       }
       return(
         <div className="main">
-        < div className='appTest'>
+        <div className='appTest'>
           {showScore? (
 
              
-                
+               <> 
             <div className='score-Section'>
               <div>
             <img src={crown} alt="Can't load." className="pl-10"></img> 
-              <p> You scored {score} out of {question.length}</p></div>
-              </div>
+              <p> You scored {score-1} out of {question.length-1}</p></div>
 
+              </div>
+              <div className="float-right ml-12">
+              <button className="w-max h-max text-white bg-blue-900 rounded-2xl border-4 p-2 border-blue-500 hover:opacity-50 px-2 shadow-emerald-500" onClick={restart}>Try Again</button>
+</div>
+              </>
               
 
           ):(
             <>
             
               <div className='question-section'>
-                <div className='question-count'>
-                  <span>Question{currentQuestion+1}</span>/{question.length}
+                <div className={visiblity} id="question-count">
+                  <span >Question{currentQuestion}</span>/{question.length-1}
                 </div>
                 <div className='question-text'>{question[currentQuestion].questionText}</div>
               </div>
     
               <div className='answer-section'>
-                {question[currentQuestion].answersOptions.map((answersOption) => (
-                <button className="btn" onClick={()=>handleButtonClick(answersOption.isCorrect)}>{answersOption.answerText}</button>))}
+                {question[currentQuestion].answersOptions.map((answersOption,index) => (
+                <button className="btn" id={index} onClick={(event)=>handleButtonClick(answersOption.isCorrect,event)}>{answersOption.answerText}</button>))}
+                <button className={visiblity} id="nxt" onClick={nextQue}>Next</button>
               </div>
+              
               
             </>
           )}
