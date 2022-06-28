9a46797ae90f7ff2860dc9a9e4e71f9232a59191
Shahnwaz Khan <93175779+khanshahnwaz@users.noreply.github.com>
2022-06-28 10:52:04 +0530

    Revert "Revert "Modified Feedback Form""

--- insertion_sort/src/Components/FeedbackForm.jsx
+++ insertion_sort/src/Components/FeedbackForm.jsx
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import { Form, Input, TextArea, Button } from 'semantic-ui-react';
 import emailjs from 'emailjs-com';
 import Swal from 'sweetalert2';
+import StarRating from './StarRating';
 import "./style.css"
 
 
@@ -73,6 +74,13 @@ const FeedbackForm = () =>{
     <div className="mb-4">
       <label className="block text-gray-700 text-sm font-bold mb-2" for="expno">
         Number of Experiments Performed
+      </label>
+      <Input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="expno" id="expno" type="text"/>
+    </div>
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="q1">
+        Your Experience
+        <StarRating/>
       </label>
       <Input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" name="expno" id="expno" type="text"/>
     </div>
